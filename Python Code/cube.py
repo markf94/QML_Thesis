1ef7a65fb10e974811a61c731e76edea913215cf
markf94 <markfingerhuth@protonmail.com>
2016-11-09 14:15:53 +0200

    tesseract plot and gaussian plots ready

--- Python Code/cube.py
+++ Python Code/cube.py
@@ -2,6 +2,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 from itertools import product, combinations
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+
 
 ################## 1ST PLOT #######################
 # INITIAL STATE
@@ -18,14 +21,14 @@ for s, e in combinations(np.array(list(product(r,r,r))), 2):
         ax.plot3D(*zip(s,e), color="b")
 
 # Label the edges
-ax.text(1,1,1,  '%s' % ("|000>\np=0.0"), size=20, zorder=1,color='k')
-ax.text(1,-1,1,  '%s' % ("|001>\np=0.0"), size=20, zorder=1,color='k')
-ax.text(1,-1,-1,  '%s' % ("|011>\np=0.0"), size=20, zorder=1,color='k')
-ax.text(1,1,-1,  '%s' % ("|010>\np=1.0"), size=20, zorder=1,color='k')
-ax.text(-1,-1,1,  '%s' % ("|101>\np=0.0"), size=20, zorder=1,color='k')
-ax.text(-1,1,1,  '%s' % ("|100>\np=0.0"), size=20, zorder=1,color='k')
-ax.text(-1,1,-1,  '%s' % ("|110>\np=0.0"), size=20, zorder=1,color='k')
-ax.text(-1,-1,-1,  '%s' % ("|111>\np=0.0"), size=20, zorder=1,color='k')
+ax.text(1,1,1,  '%s' % ("|000>\np=0.0"), size=14, zorder=1,color='k')
+ax.text(1,-1,1,  '%s' % ("|001>\np=0.0"), size=14, zorder=1,color='k')
+ax.text(1,-1,-1,  '%s' % ("|011>\np=0.0"), size=14, zorder=1,color='k')
+ax.text(1,1,-1,  '%s' % ("|010>\np=1.0"), size=14, zorder=1,color='k')
+ax.text(-1,-1,1,  '%s' % ("|101>\np=0.0"), size=14, zorder=1,color='k')
+ax.text(-1,1,1,  '%s' % ("|100>\np=0.0"), size=14, zorder=1,color='k')
+ax.text(-1,1,-1,  '%s' % ("|110>\np=0.0"), size=14, zorder=1,color='k')
+ax.text(-1,-1,-1,  '%s' % ("|111>\np=0.0"), size=14, zorder=1,color='k')
 
 # Hide the axes
 ax = plt.gca(projection='3d')
@@ -49,18 +52,19 @@ for s, e in combinations(np.array(list(product(r,r,r))), 2):
         ax.plot3D(*zip(s,e), color="b")
 
 # Label the edges
-ax.text(1,1,1,  '%s' % ("|000>\np=0.009210"), size=20, zorder=1,color='k')
-ax.text(1,-1,1,  '%s' % ("|001>\np=0.080990"), size=20, zorder=1,color='k')
-ax.text(1,-1,-1,  '%s' % ("|011>\np=0.008800"), size=20, zorder=1,color='k')
-ax.text(1,1,-1,  '%s' % ("|010>\np=0.001000"), size=20, zorder=1,color='k')
-ax.text(-1,-1,1,  '%s' % ("|101>\np=0.728780"), size=20, zorder=1,color='k')
-ax.text(-1,1,1,  '%s' % ("|100>\np=0.081040"), size=20, zorder=1,color='k')
-ax.text(-1,1,-1,  '%s' % ("|110>\np=0.009150"), size=20, zorder=1,color='k')
-ax.text(-1,-1,-1,  '%s' % ("|111>\np=0.081030"), size=20, zorder=1,color='k')
+ax.text(1,1,1,  '%s' % ("|000>\np=0.009210"), size=14, zorder=1,color='k')
+ax.text(1,-1,1,  '%s' % ("|001>\np=0.080990"), size=14, zorder=1,color='k')
+ax.text(1,-1,-1,  '%s' % ("|011>\np=0.008800"), size=14, zorder=1,color='k')
+ax.text(1,1,-1,  '%s' % ("|010>\np=0.001000"), size=14, zorder=1,color='k')
+ax.text(-1,-1,1,  '%s' % ("|101>\np=0.728780"), size=14, zorder=1,color='k')
+ax.text(-1,1,1,  '%s' % ("|100>\np=0.081040"), size=14, zorder=1,color='k')
+ax.text(-1,1,-1,  '%s' % ("|110>\np=0.009150"), size=14, zorder=1,color='k')
+ax.text(-1,-1,-1,  '%s' % ("|111>\np=0.081030"), size=14, zorder=1,color='k')
 
 # Hide the axes
 ax = plt.gca(projection='3d')
 ax._axis3don = False
+
 
 # Show the figure
 plt.show()
@@ -80,14 +84,14 @@ for s, e in combinations(np.array(list(product(r,r,r))), 2):
         ax.plot3D(*zip(s,e), color="b")
 
 # Label the edges
-ax.text(1,1,1,  '%s' % ("|000>\np=0.081570"), size=20, zorder=1,color='k')
-ax.text(1,-1,1,  '%s' % ("|001>\np=0.008790"), size=20, zorder=1,color='k')
-ax.text(1,-1,-1,  '%s' % ("|011>\np=0.078770"), size=20, zorder=1,color='k')
-ax.text(1,1,-1,  '%s' % ("|010>\np=0.730840"), size=20, zorder=1,color='k')
-ax.text(-1,-1,1,  '%s' % ("|101>\np=0.001100"), size=20, zorder=1,color='k')
-ax.text(-1,1,1,  '%s' % ("|100>\np=0.009110"), size=20, zorder=1,color='k')
-ax.text(-1,1,-1,  '%s' % ("|110>\np=0.081000"), size=20, zorder=1,color='k')
-ax.text(-1,-1,-1,  '%s' % ("|111>\np=0.008820"), size=20, zorder=1,color='k')
+ax.text(1,1,1,  '%s' % ("|000>\np=0.081570"), size=14, zorder=1,color='k')
+ax.text(1,-1,1,  '%s' % ("|001>\np=0.008790"), size=14, zorder=1,color='k')
+ax.text(1,-1,-1,  '%s' % ("|011>\np=0.078770"), size=14, zorder=1,color='k')
+ax.text(1,1,-1,  '%s' % ("|010>\np=0.730840"), size=14, zorder=1,color='k')
+ax.text(-1,-1,1,  '%s' % ("|101>\np=0.001100"), size=14, zorder=1,color='k')
+ax.text(-1,1,1,  '%s' % ("|100>\np=0.009110"), size=14, zorder=1,color='k')
+ax.text(-1,1,-1,  '%s' % ("|110>\np=0.081000"), size=14, zorder=1,color='k')
+ax.text(-1,-1,-1,  '%s' % ("|111>\np=0.008820"), size=14, zorder=1,color='k')
 
 # Hide the axes
 ax = plt.gca(projection='3d')
@@ -96,28 +100,80 @@ ax._axis3don = False
 # Show the figure
 plt.show()
 
-####################### 4TH PLOT ###########################
-# Show that it distributes in a Gaussian fashion
-# 3 qubit case
+################## 4TH PLOT #######################
+# Tesseract (hypercube) for 4 qubit system
 
-x = [-3,-2,-1,0,1,2,3]
-#order:
-# |101>,|001>,|011>,|010>,|000>,|100>,|101>
-y = [0.001100,0.008790,0.078770,0.730840,0.081570,0.009110,0.001100]
-plt.plot(x,y)
-plt.ylabel('Probability')
-plt.xlabel('abs(x) = Hamming distance')
-plt.show()
+# Initialize the 3D figure
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_aspect("equal")
 
-####################### 5TH PLOT ###########################
-# Show that it distributes in a Gaussian fashion
-# 4 qubit case
+# Draw first (smaller) cube
+r = [-1, 1]
+for s, e in combinations(np.array(list(product(r,r,r))), 2):
+    if np.sum(np.abs(s-e)) == r[1]-r[0]:
+        ax.plot3D(*zip(s,e), color="b")
 
-x = [-4,-3,-2,-1,0,1,2,3,4]
-#order:
-# 4:HD |1011>,3HD: |0011>, 2HD: |0111>,1HD: |0110>, 0HD: |0100>, 1HD: |0000>, 2HD: |1110>, 3HD: |1111>, 4HD: |1011>
-y = [0.000100, 0.000880, 0.008090, 0.073020,  0.653810 , 0.072500 , 0.007950, 0.000870 , 0.000100]
-plt.plot(x,y)
-plt.ylabel('Probability')
-plt.xlabel('abs(x) = Hamming distance')
+# Label the edges
+
+#inner cube
+ax.text(1,1,1,  '%s' % ("|0000>"), size=14, zorder=1,color='k')
+ax.text(1,-1,1,  '%s' % ("|0001>"), size=14, zorder=1,color='k')
+ax.text(1,-1,-1,  '%s' % ("|1001>"), size=14, zorder=1,color='k')
+ax.text(1,1,-1,  '%s' % ("|1000>"), size=14, zorder=1,color='k')
+ax.text(-1,-1,1,  '%s' % ("|0011>"), size=14, zorder=1,color='k')
+ax.text(-1,1,1,  '%s' % ("|0010>"), size=14, zorder=1,color='k')
+ax.text(-1,1,-1,  '%s' % ("|1010>"), size=14, zorder=1,color='k')
+ax.text(-1,-1,-1,  '%s' % ("|1011>"), size=14, zorder=1,color='k')
+#outer cube
+ax.text(-2,-2,-2,  '%s' % ("|1111>"), size=14, zorder=1,color='k')
+ax.text(2,-2,-2,  '%s' % ("|1101>"), size=14, zorder=1,color='k')
+ax.text(-2,2,2,  '%s' % ("|0110>"), size=14, zorder=1,color='k')
+ax.text(-2,2,-2,  '%s' % ("|1110>"), size=14, zorder=1,color='k')
+ax.text(-2,-2,2,  '%s' % ("|0111>"), size=14, zorder=1,color='k')
+ax.text(2,2,2,  '%s' % ("|0100>"), size=14, zorder=1,color='k')
+ax.text(2,2,-2,  '%s' % ("|1100>"), size=14, zorder=1,color='k')
+ax.text(2,-2,2,  '%s' % ("|0101>"), size=14, zorder=1,color='k')
+
+# Hide the axes
+ax = plt.gca(projection='3d')
+ax._axis3don = False
+
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer)
+
+# Define the vectors for the lines
+a = Arrow3D([-1,-2],[-1,-2],[-1,-2], mutation_scale=20, lw=1, arrowstyle="-", color="b")
+b = Arrow3D([1,2],[-1,-2],[-1,-2], mutation_scale=20, lw=1, arrowstyle="-", color="b")
+c = Arrow3D([-1,-2],[1,2],[-1,-2], mutation_scale=20, lw=1, arrowstyle="-", color="b")
+d = Arrow3D([-1,-2],[-1,-2],[1,2], mutation_scale=20, lw=1, arrowstyle="-", color="b")
+e = Arrow3D([1,2],[1,2],[-1,-2], mutation_scale=20, lw=1, arrowstyle="-", color="b")
+f = Arrow3D([-1,-2],[1,2],[1,2], mutation_scale=20, lw=1, arrowstyle="-", color="b")
+g = Arrow3D([1,2],[-1,-2],[1,2], mutation_scale=20, lw=1, arrowstyle="-", color="b")
+h = Arrow3D([1,2],[1,2],[1,2], mutation_scale=20, lw=1, arrowstyle="-", color="b")
+
+ax.add_artist(a)
+ax.add_artist(b)
+ax.add_artist(c)
+ax.add_artist(d)
+ax.add_artist(e)
+ax.add_artist(f)
+ax.add_artist(g)
+ax.add_artist(h)
+
+# Draw second cube (bigger one)
+r = [-2, 2]
+for s, e in combinations(np.array(list(product(r,r,r))), 2):
+    if np.sum(np.abs(s-e)) == r[1]-r[0]:
+        ax.plot3D(*zip(s,e), color="b")
+
+# Show the figure
 plt.show()
---
+++ Python Code/gaussiandiffusion.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+## This script plots the absolute value of the Hamming distance versus
+# probability when using the diffusion operator by Schuld et al. (inspired by
+# quantum random walks)
+
+####################### 1ST PLOT ###########################
+# Show that it distributes in a Gaussian fashion
+# 3 qubit case with delta = 0.9
+
+x = [-3,-2,-1,0,1,2,3]
+#order:
+# |101>,|001>,|011>,|010>,|000>,|100>,|101>
+y = [0.001100,0.008790,0.078770,0.730840,0.081570,0.009110,0.001100]
+plt.plot(x,y)
+plt.ylim(0,1)
+plt.ylabel('Probability')
+plt.xlabel('abs(x) = Hamming distance')
+plt.title('3 qubit case with delta=0.9')
+plt.show()
+
+####################### 2ND PLOT ###########################
+# Show that it distributes in a Gaussian fashion
+# 4 qubit case with delta = 0.9
+
+x = [-4,-3,-2,-1,0,1,2,3,4]
+#order:
+# 4:HD |1011>,3HD: |0011>, 2HD: |0111>,1HD: |0110>, 0HD: |0100>, 1HD: |0000>, 2HD: |1110>, 3HD: |1111>, 4HD: |1011>
+y = [0.000100, 0.000880, 0.008090, 0.073020,  0.653810 , 0.072500 , 0.007950, 0.000870 , 0.000100]
+plt.plot(x,y)
+plt.ylim(0,1)
+plt.ylabel('Probability')
+plt.xlabel('abs(x) = Hamming distance')
+plt.title('4 qubit case with delta=0.9')
+plt.show()
+
+####################### 3RD PLOT ###########################
+# Show that it distributes in a Gaussian fashion
+# 4 qubit case with delta=0.8
+
+x = [-4,-3,-2,-1,0,1,2,3,4]
+#order:
+# 4:HD |1011>,3HD: |0011>, 2HD: |0111>,1HD: |0110>, 0HD: |0100>, 1HD: |0000>, 2HD: |1110>, 3HD: |1111>, 4HD: |1011>
+y = [0.001600, 0.006500, 0.025810, 0.103850,  0.407530 , 0.101520 , 0.025120, 0.006420 , 0.001600]
+plt.plot(x,y)
+plt.ylim(0,1)
+plt.ylabel('Probability')
+plt.xlabel('abs(x) = Hamming distance')
+plt.title('4 qubit case with delta=0.8')
+plt.show()
+
+####################### 4TH PLOT ###########################
+# Show that it distributes in a Gaussian fashion
+# 4 qubit case with delta=0.7
+
+x = [-4,-3,-2,-1,0,1,2,3,4]
+#order:
+# 4:HD |1011>,3HD: |0011>, 2HD: |0111>,1HD: |0110>, 0HD: |0100>, 1HD: |0000>, 2HD: |1110>, 3HD: |1111>, 4HD: |1011>
+y = [0.007920, 0.018960, 0.043210, 0.101660,  0.239850 , 0.104090 , 0.044750, 0.018530 , 0.007920]
+plt.plot(x,y)
+plt.ylim(0,1)
+plt.ylabel('Probability')
+plt.xlabel('abs(x) = Hamming distance')
+plt.title('4 qubit case with delta=0.7')
+plt.show()
+
+
+####################### 5TH PLOT ###########################
+# Show that it distributes in a Gaussian fashion
+# 4 qubit case with delta=0.6
+
+x = [-4,-3,-2,-1,0,1,2,3,4]
+#order:
+# 4:HD |1011>,3HD: |0011>, 2HD: |0111>,1HD: |0110>, 0HD: |0100>, 1HD: |0000>, 2HD: |1110>, 3HD: |1111>, 4HD: |1011>
+y = [0.025390, 0.038630, 0.058350, 0.085750,  0.129960 , 0.087560 , 0.056280, 0.038770 , 0.025390]
+plt.plot(x,y)
+plt.ylim(0,1)
+plt.ylabel('Probability')
+plt.xlabel('abs(x) = Hamming distance')
+plt.title('4 qubit case with delta=0.6')
+plt.show()
+
+
+####################### 6TH PLOT ###########################
+# Show that it distributes in a Gaussian fashion
+# 4 qubit case with delta=0.5
+
+x = [-4,-3,-2,-1,0,1,2,3,4]
+#order:
+# 4:HD |1011>,3HD: |0011>, 2HD: |0111>,1HD: |0110>, 0HD: |0100>, 1HD: |0000>, 2HD: |1110>, 3HD: |1111>, 4HD: |1011>
+y = [0.063610, 0.063770, 0.062440, 0.061800,  0.063010 , 0.062780 , 0.062690, 0.061890 , 0.063610]
+plt.plot(x,y)
+plt.ylim(0,1)
+plt.ylabel('Probability')
+plt.xlabel('abs(x) = Hamming distance')
+plt.title('4 qubit case with delta=0.5')
+plt.show()
